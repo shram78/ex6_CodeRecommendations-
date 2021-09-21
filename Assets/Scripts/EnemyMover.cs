ad155903560ffde995b6e8ddd52ab15ce895e009
shram78 <ra00275@gmail.com>
2021-09-21 23:52:49 +0300

    spawn, move, die complete

---
+++ Assets/Scripts/EnemyMover.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+ 
+    private void Update()
+    {
+        transform.Translate(_speed * Time.deltaTime, 0, 0);
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _enemyPrefab;
+    [SerializeField] private Transform[] _spawnPoints;
+    [SerializeField] private float _secondBetweenSpawn;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnEnemy());
+    }
+
+    private IEnumerator SpawnEnemy()
+    {
+        var waitForPerSeconds = new WaitForSeconds(_secondBetweenSpawn);
+
+        do
+        {
+            for (int i = 0; i < _spawnPoints.Length; i++)
+            {
+                Instantiate(_enemyPrefab, _spawnPoints[i]);
+
+                yield return waitForPerSeconds;
+            }
+        } while (true); //для ментора: умышленно оставил бесконечный цикл, тк по условиям задания нет условий окончания спауна
+    }
+}
